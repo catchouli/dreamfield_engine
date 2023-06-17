0ce427cfafdef06c78ed255a9de49bc181a8cf8b
Caitlin Wilks <cat@cat.bio>
2023-06-17 14:37:27 +0100

    remove app stuff

--- src/app_state.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
-pub enum AppState {
-    SplashScreen,
-    TitleScreen,
-    MainGame,
-    Paused,
-}
--- src/states.rs
+++
@@ -1,4 +0,0 @@
-pub mod splash_screen;
-pub mod title_screen;
-pub mod main_game;
-pub mod pause_menu;
